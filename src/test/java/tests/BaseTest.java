bbafbbacf2eaa98f5c86907c59cd345b38e970a0
Test DG User <Test DG Email>
2024-11-16 19:10:57 +0300

    Added tests - vacancy page

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,44 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class BaseTest {
+    @BeforeAll
+    static void setUp() {
+        Configuration.browser = System.getProperty("browserName", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.remote = System.getProperty("selenoidUrl", null);
+        Configuration.baseUrl = "https://ozerki.ru/vacancies/";
+        Configuration.pageLoadStrategy = "eager";
+        //Configuration.holdBrowserOpen = true;
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterEach
+    void afterEach() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
+    }
+}
---
+++ src/test/java/tests/VacancyPageTests.java
@@ -0,0 +1,60 @@
+package tests;
+
+import io.qameta.allure.Issue;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import pages.VacancyPage;
+
+public class VacancyPageTests extends BaseTest {
+
+    VacancyPage page = new VacancyPage();
+
+    @Test
+    @Severity(SeverityLevel.NORMAL)
+    @Issue("AUT-1234")
+    @Owner("Denis Gubert")
+    @DisplayName("Проверка Title на странице Вакансий")
+    void shouldHaveVacancyTitle() {
+        page.openVacancyPage();
+
+        page.shoudHaveTitle("Вакансии | Озерки");
+    }
+
+    @Severity(SeverityLevel.CRITICAL)
+    @Issue("AUT-1235")
+    @Owner("Denis Gubert")
+    @ParameterizedTest(name = "Проверка наличия вакансии {0}")
+    @ValueSource( strings = {
+            "Фармацевт-провизор (Приладожский 23А)",
+            "Фармацевт-провизор (ул., Пейзажная, д.2, к.1)",
+            "Фармацевт-провизор (м. Беляево) новая аптека"
+        }
+    )
+    void shouldHaveVacancies(String vacancy) {
+        page.openVacancyPage();
+
+        page.shoudHaveVacancy(vacancy);
+    }
+
+    @Severity(SeverityLevel.CRITICAL)
+    @Issue("AUT-1236")
+    @Owner("Denis Gubert")
+    @ParameterizedTest(name = "Проверка поиска вакансии по значению {0}")
+    @ValueSource( strings = {
+            "Приладожский",
+            "Пейзажная",
+            "Беляево"
+        }
+    )
+    void shouldHaveVacanciesAfterSearch(String searchString) throws InterruptedException {
+        page.openVacancyPage()
+                .filterVacanciesByString(searchString);
+
+        page.shoudHaveStringInVacancies(searchString);
+    }
+}
